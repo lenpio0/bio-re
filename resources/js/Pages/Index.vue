457bfbcb2fb49b2a057f448d3f1a93bfd0b102be
lenpio0 <127386860+lenpio0@users.noreply.github.com>
2023-07-10 09:17:09 +0200

    Initial commit

---
+++ resources/js/Pages/Index.vue
@@ -0,0 +1,137 @@
+<template>
+    <div class="bg-white-bg text-black-text">
+        <header class="bg-cover bg-fixed bg-[url(https://media.gettyimages.com/id/1366257751/fr/vectoriel/fond-de-doodle-de-flux-abstrait.jpg?s=612x612&w=gi&k=20&c=cMUQqytgsEro5CIfN9W4LyjEzhbniMGrPAHN3Gkv6g4=)]">
+            <nav>
+                <ul class="z-20 mt-4 flex rounded-md w-[95%] max-w-6xl h-12 items-center justify-evenly bg-yellow-pale drop-shadow fixed " style="position: fixed; left: 50%; transform: translateX(-50%);">
+                    <li><a>Accueil</a></li>
+                    <span class="opacity-30">|</span>
+                    <li><a>Bio-résonance</a></li>
+                    <span class="opacity-30">|</span>
+                    <li><a>Reiki</a></li>
+                    <span class="opacity-30">|</span>
+                    <li><a>Lahochi</a></li>
+                    <span class="opacity-30">|</span>
+                    <li><a>Tarifs</a></li>
+                    <span class="opacity-30">|</span>
+                    <li><a>Contact</a></li>
+                </ul>
+            </nav>
+        </header>
+        <section>
+            <div class="bg-cover bg-fixed bg-[url(https://media.gettyimages.com/id/1366257751/fr/vectoriel/fond-de-doodle-de-flux-abstrait.jpg?s=612x612&w=gi&k=20&c=cMUQqytgsEro5CIfN9W4LyjEzhbniMGrPAHN3Gkv6g4=)]">
+                <div class="pt-16 flex space-between w-full backdrop-blur-sm">
+                    <img src="https://placehold.co/800x600/F9E3D6/403233" class="h-[30rem] opacity-0">
+                    <div class="flex flex-col w-full p-12 justify-center bg-white-bg m-16 rounded-3xl">
+                        <h1 class="mx-auto text-4xl">Bio-résonance & Bien-être</h1>
+                        <p class="mx-auto mb-8 mt-4">Sophie Guilgot, praticienne en Bio-résonance</p>
+                        <p class="mx-8">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <div class="bg-cover bg-fixed bg-[url(https://media.gettyimages.com/id/1366257751/fr/vectoriel/fond-de-doodle-de-flux-abstrait.jpg?s=612x612&w=gi&k=20&c=cMUQqytgsEro5CIfN9W4LyjEzhbniMGrPAHN3Gkv6g4=)]">
+            <div class="bg-gradient-to-b from-transparent to-white-bg h-24 w-full backdrop-blur-sm"></div>
+        </div>
+        <section class="h-[36rem] mx-auto mb-36 flex flex-wrap px-24 justify-between">
+            <h2 class="mx-auto mt-24 w-full text-center text-4xl">Bio-résonance</h2>
+            <div class="bg-pink-champ px-4 pt-6 pb-4 my-8 rounded-xl flex flex-col justify-between max-w-lg">
+                <h3 class="mx-auto text-center text-xl max-w-7xl">Qu'est-ce que la biorésonance ?</h3>
+                <p class="max-w-7xl mx-auto">La biorésonance est une approche thérapeutique qui repose sur le principe que chaque organisme vivant émet des vibrations électromagnétiques spécifiques. Elle vise à détecter et à corriger les déséquilibres énergétiques dans le corps en utilisant des appareils spécialisés.</p>
+            </div>
+            <div class="bg-pink-champ px-4 pt-6 pb-4 my-8 rounded-xl flex flex-col justify-between max-w-lg">
+                <h3 class="mx-auto text-center text-xl max-w-7xl">Comment fonctionne la biorésonance ?</h3>
+                <p class="max-w-7xl mx-auto">La biorésonance fonctionne en mesurant les fréquences électromagnétiques émises par le corps à l'aide d'appareils tels que des analyseurs de résonance magnétique. Ces fréquences sont ensuite évaluées pour identifier les éventuels déséquilibres énergétiques. Des dispositifs de rétroaction sont ensuite utilisés pour envoyer des signaux électromagnétiques spécifiques au corps afin de rétablir l'harmonie énergétique.</p>
+            </div>
+            <div class="bg-pink-champ px-4 pt-6 pb-4 my-8 rounded-xl flex flex-col justify-between max-w-lg">
+                <h3 class="mx-auto text-center text-xl  max-w-7xl">Quels sont les avantages de la biorésonance ?</h3>
+                <p class="max-w-7xl mx-auto">La biorésonance offre certains avantages. Elle est souvent considérée comme une approche non invasive, ce qui signifie qu'elle n'implique généralement pas de procédures douloureuses ou traumatisantes pour le corps. la biorésonance peut être utilisée pour aborder une large gamme de problèmes de santé, allant des allergies aux troubles du sommeil en passant par les douleurs chroniques, ou comme complément à d'autres approches médicales.</p>
+            </div>
+        </section>
+        <div class="bg-gradient-to-b from-white-bg to-pink-champ h-1 w-full"></div>
+        <section class="h-[48rem] mx-auto pb-36 flex flex-wrap px-24 bg-pink-champ">
+            <h2 class="mx-auto mt-24 w-full text-center text-4xl">Massage énergétique</h2>
+            <div class="flex flex-wrap justify-around w-full">
+                <div class="w-[45%] bg-white-bg px-4 pt-6 pb-4 my-8 rounded-xl flex flex-col justify-between max-w-lg">
+                    <h3 class="mx-auto text-center text-xl max-w-7xl">Qu'est-ce que le massage énergétique ?</h3>
+                    <p>Le massage énergétique est une pratique thérapeutique qui se concentre sur l'énergie vitale qui circule à travers le corps. Il vise à équilibrer et à harmoniser les flux d'énergie pour favoriser la guérison et le bien-être. Le massage énergétique considère l'aspect énergétique de l'être humain, en travaillant sur les canaux énergétiques, les centres d'énergie (chakras) et les méridiens du corps.</p>
+                </div>
+                <div class="w-[45%] bg-white-bg px-4 pt-6 pb-4 my-8 rounded-xl flex flex-col justify-between max-w-lg">
+                    <h3 class="mx-auto text-center text-xl max-w-7xl">Comment se déroule une séance ?</h3>
+                    <p>Lors d'une séance de massage énergétique, le praticien utilise ses mains pour manipuler l'énergie autour du corps du receveur. Cela peut impliquer des mouvements de balayage, des pressions légères ou des touchers doux sur des points spécifiques du corps. Le praticien travaille à libérer les blocages énergétiques, à rétablir l'équilibre et à stimuler la circulation de l'énergie vitale dans le corps du receveur.</p>
+                </div>
+                <div class="w-[45%] bg-white-bg px-4 pt-6 pb-4 my-8 rounded-xl flex flex-col justify-between max-w-lg">
+                    <h3 class="mx-auto text-center text-xl max-w-7xl">Quels en sont les bienfaits ?</h3>
+                    <p>Le massage énergétique peut apporter de nombreux bienfaits. Il peut aider à réduire le stress, à soulager les tensions musculaires, à améliorer la circulation sanguine et lymphatique, à favoriser la détente profonde, à stimuler le système immunitaire et à équilibrer les émotions. Le massage énergétique peut également aider à libérer les blocages énergétiques qui peuvent contribuer à des problèmes physiques, émotionnels ou mentaux, permettant ainsi une meilleure harmonie et un sentiment global de bien-être.</p>
+                </div>
+            </div>
+        </section>
+        <div class="bg-gradient-to-b from-pink-champ to-white-bg h-1 w-full"></div>
+        <section class="h-[30rem] bg-white-bg mx-auto px-48 mb-16">
+            <h2 class="mx-auto pt-24 pb-16 w-96 text-center text-4xl">Tarifs</h2>
+            <div class="flex mx-auto justify-around">
+                <div class="w-2/5 bg-pink-champ px-4 pt-6 pb-4 my-8 rounded-xl flex flex-col justify-between max-w-xl">
+                    <h3 class="mx-auto text-center text-xl max-w-7xl">Bio-résonance</h3>
+                    <p>Consultation initiale (1,5 heures) : 70 €</p>
+                    <p>Séance individuelle de bio résonance (1 heure) : 60 €</p>
+                    <p>Forfait bio résonance (3 séances) : 150 €</p>
+                </div>
+                <div class="w-2/5 bg-pink-champ px-4 pt-6 pb-4 my-8 rounded-xl flex flex-col justify-between max-w-xl">
+                    <h3 class="mx-auto text-center text-xl max-w-7xl">Lahochi</h3>
+                    <p>Séance individuelle de Lahochi (1 heure) : 50 €</p>
+                    <p>Forfait Lahochi (3 séances) : 120 €</p>
+                    <p>Séance de suivi en Lahochi (30 minutes) : 35 €</p>
+                </div>
+            </div>
+        </section>
+        <div class="bg-cover bg-fixed bg-[url(https://media.gettyimages.com/id/1366257751/fr/vectoriel/fond-de-doodle-de-flux-abstrait.jpg?s=612x612&w=gi&k=20&c=cMUQqytgsEro5CIfN9W4LyjEzhbniMGrPAHN3Gkv6g4=)]">
+            <div class="bg-gradient-to-b from-white-bg to-[transparent] h-24 w-full backdrop-blur-sm"></div>
+        </div>
+        <section class="bg-cover bg-fixed bg-[url(https://media.gettyimages.com/id/1366257751/fr/vectoriel/fond-de-doodle-de-flux-abstrait.jpg?s=612x612&w=gi&k=20&c=cMUQqytgsEro5CIfN9W4LyjEzhbniMGrPAHN3Gkv6g4=)]">
+            <div class="h-[40rem] flex flex-wrap justify-around mx-auto px-48 backdrop-blur-sm">
+                <h2 class="w-full text-4xl text-center pt-24">Contact</h2>
+                <div class="w-2/5 bg-white-bg px-4 pt-6 pb-4 my-8 rounded-xl flex flex-col justify-between max-w-xl">
+                    <h3 class="text-2xl text-center">Sophie Guilgot</h3>
+                    <div>
+                        <div class="flex justify-around w-3/4 mx-auto">
+                            <p class="text-lg">Téléphone :</p>
+                            <p class="text-lg">06 12 34 56 78</p>
+                        </div>
+                        <div class="flex justify-around w-3/4 mx-auto mt-2">
+                            <p class="text-lg">Email :</p>
+                            <p class="text-lg">blablabla@gmail.com</p>
+                        </div>
+                    </div>
+                    <div class="flex justify-around mx-auto w-1/2 mb-3">
+                        <a href="#"><svg xmlns="http://www.w3.org/2000/svg" width="3rem" height="3rem" viewBox="0 0 24 24"><g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"><path d="M21 8v8a5 5 0 0 1-5 5H8a5 5 0 0 1-5-5V8a5 5 0 0 1 5-5h8a5 5 0 0 1 5 5Z"/><path d="M10 12a3 3 0 1 0 3 3V6c.333 1 1.6 3 4 3"/></g></svg></a>
+                        <a href="#"><svg xmlns="http://www.w3.org/2000/svg" width="3rem" height="3rem" viewBox="0 0 24 24"><g fill="none" stroke="currentColor" stroke-width="1.5"><path stroke-linecap="round" stroke-linejoin="round" d="M12 16a4 4 0 1 0 0-8a4 4 0 0 0 0 8Z"/><path d="M3 16V8a5 5 0 0 1 5-5h8a5 5 0 0 1 5 5v8a5 5 0 0 1-5 5H8a5 5 0 0 1-5-5Z"/><path stroke-linecap="round" stroke-linejoin="round" d="m17.5 6.51l.01-.011"/></g></svg></a>
+                        <a href="#"><svg xmlns="http://www.w3.org/2000/svg" width="3rem" height="3rem" viewBox="0 0 24 24"><g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"><path d="M21 8v8a5 5 0 0 1-5 5H8a5 5 0 0 1-5-5V8a5 5 0 0 1 5-5h8a5 5 0 0 1 5 5Z"/><path d="M11 21v-9c0-2.188.5-4 4-4m-6 5h6"/></g></svg></a>
+                    </div>
+                </div>
+                <form class="w-2/5 bg-white-bg px-4 pt-6 pb-4 my-8 rounded-xl max-w-xl">
+                    <div class="flex flex-wrap justify-around">
+                        <input type="text" placeholder="Prénom" class="w-[45%] mb-4 rounded-md">
+                        <input type="text" placeholder="Nom" class="w-[45%] mb-4 rounded-md">
+                        <input type="email" placeholder="Email" class="w-[45%] mb-4 rounded-md">
+                        <input type="tel" placeholder="Téléphone" class="w-[45%] mb-4 rounded-md">
+                        <textarea placeholder="Écrivez ici votre message ici..." class="w-[95%] rounded-md"></textarea>
+                    </div>
+                    <input type="submit" class="mx-auto block bg-pink-dark rounded-md py-2 px-4 mt-6">
+                </form>
+            </div>
+        </section>
+        <footer class="py-4 bg-yellow-pale">
+            <div class="flex justify-around px-2 text-sm max-w-7xl mx-auto">
+                <a>Termes et conditions</a>
+                <a>Politique de cookies</a>
+                <a>Mentions légales</a>
+                <a>Politique de confidentialité</a>
+            </div>
+            <p class="text-center pt-4 text-sm">© 2023 Bio-Résonance & Bien-Être</p>
+            <p></p>
+        </footer>
+    </div>
+</template>
+
+<script>
+
+</script>
---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+import forms from '@tailwindcss/forms';
+import typography from '@tailwindcss/typography';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './vendor/laravel/jetstream/**/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+        './resources/js/**/*.vue',
+    ],
+
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+            },
+            colors: {
+                'black-text': '#403233',
+                'white-bg': '#FFF7F3',
+                'pink-champ': '#F9E3D6',
+                'pink-dark': '#EFC7C2',
+                'yellow-pale': '#FDDDBC',
+                'green-accent': '#DDE5B6'
+            }
+        },
+    },
+
+    plugins: [forms, typography],
+};
